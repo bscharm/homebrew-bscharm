0748d8b22913fc70543c871e31423ac006926575
Brad Scharmann <bscharm@gmail.com>
2016-02-25 13:42:43 -0700

    first attempt at formula

---
+++ Formula/bgrok.rb
@@ -0,0 +1,17 @@
+class Bgrok < Formula
+  desc "Self hosted fork of ngrok at bscharm.com"
+  homepage "https://github.com/bscharm/bgrok"
+  url "https://github.com/bscharm/bgrok/archive/1.7.2.tar.gz"
+  sha256 "43a3c57343b9130264e01183948490ebcbdb83f827fa9fccca53b8491689f8dc"
+  depends_on :x11
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "release-client"
+  end
+  test do
+    system "false"
+  end
+end
