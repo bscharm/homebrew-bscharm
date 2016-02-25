616fd1196adac79e08d19375625c8c87bed2382b
Brad Scharmann <bscharm@gmail.com>
2016-02-25 13:49:04 -0700

    remove default formula

--- Formula/bgrok.rb
+++ Formula/bgrok.rb
@@ -4,10 +4,6 @@ class Bgrok < Formula
   url "https://github.com/bscharm/bgrok/archive/1.7.2.tar.gz"
   sha256 "43a3c57343b9130264e01183948490ebcbdb83f827fa9fccca53b8491689f8dc"
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
     system "make", "release-client"
     bin.install "bin/bgrok"
   end
